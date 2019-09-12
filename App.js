38bce203db32f6bfc004b69245ec9b8a1d0bfe66
Turiano Romero <romero.guzman18@gmail.com>
2019-09-12 09:18:11 -0500

    Styles form

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, KeyboardAvoidingView } from 'react-native';
+import From from './components/Form';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <KeyboardAvoidingView style={styles.container} behavior='padding'>
+      <From/>
+    </KeyboardAvoidingView>
   );
 }
 
